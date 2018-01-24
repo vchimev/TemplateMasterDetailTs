ae9ef65e8cac4e1f983137f26400b203a2884ab7
vchimev <Vasil.Chimev@gmail.com>
2018-01-24 11:31:56 +0200

    init: app

---
+++ app/app.ts
@@ -0,0 +1,25 @@
+import * as app from "application";
+
+import "./bundle-config";
+
+/* ************************************************************************
+* Importing all rxjs operators increases build output and duration
+* so it is better to import the operators in use only;
+* also, due to the polyfill-ish nature of the RxJS modules,
+* it is enough to import an operator once at a single, centralized location.
+**************************************************************************/
+import "./rxjs-imports";
+
+/* ***********************************************************
+* The {N} Firebase plugin needs some initialization steps before it is ready
+* for use. Check out the initialization script at /shared/firebase.common.ts
+* along with more information about it.
+*************************************************************/
+import "./shared/firebase.common";
+
+app.start({ moduleName: "cars/cars-list-page" });
+
+/*
+Do not place any code after the application has been started as it will not
+be executed on iOS.
+*/
---
+++ app/bundle-config.ts
@@ -0,0 +1,12 @@
+if (global.TNS_WEBPACK) {
+    // registers tns-core-modules UI framework modules
+    require("bundle-entry-points");
+
+    global.registerModule("nativescript-pro-ui/listview",
+        () => require("../node_modules/nativescript-pro-ui/listview"));
+
+    // register application modules
+    // This will register each `page` postfixed xml, css, js, ts, scss in the app/ folder
+    const context = require.context("~/", true, /(page|fragment)\.(xml|css|js|ts|scss)$/);
+    global.registerWebpackModules(context);
+}
